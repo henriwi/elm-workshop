cdcede5413288008f92d9485eaf29a9dfd635fe5
Henrik Wingerei <henrik.wingerei@bekk.no>
2014-04-09 11:54:58 +0200

    Nytt bilde og liten endring i løsning på oppgav e3

--- oppgave3/oppgave3_solution.elm
+++ oppgave3/oppgave3_solution.elm
@@ -6,6 +6,6 @@ import Keyboard
 display {x,y} = collage 800 800 [
   rect width height |> filled black, 
   rect 80 10 |> filled red |> move (toFloat x*100, -halfHeight+10),
-  circle 8 |> filled white |> move (0,halfHeight-8)]
+  circle 8 |> filled white |> move (0,halfHeight)]
 
 main = lift display Keyboard.arrows
